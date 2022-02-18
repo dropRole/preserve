1f426c142a06b67c46f256e7306a32f00040a587
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-02-18 14:14:13 +0100

    Update offerees.service.ts
    Servicing HTTP Patch request for offeree username update.

--- src/offerees/offerees.service.ts
+++ src/offerees/offerees.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { OffereeAuthCredentialsDTO } from 'src/auth/dto/offeree-auth-credentials.dto';
+import { UpdateOffereeUsernameDTO } from './dto/update-offeree-username.dto';
 import { Offeree } from './offeree.entity';
 import { OffereesRepository } from './offerees.repository';
 
@@ -20,5 +21,12 @@ export class OffereesService {
   }
   getOffereeByUsername(username: string): Promise<Offeree> {
     return this.offereesRepository.selectOfferee(username);
+  }
+  updateOffereeUsername(
+    updateOffereeUsernameDTO: UpdateOffereeUsernameDTO,
+  ): Promise<void> {
+    return this.offereesRepository.updateOffereeUsername(
+      updateOffereeUsernameDTO,
+    );
   }
 }
