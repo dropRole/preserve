8a856c28b021b296fb23553f6a896ddb505a2e0f
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-02-18 17:49:14 +0100

    Update offerors.module.ts
    Provided offerors module with OfferorsRepository.

--- src/offerors/offerors.module.ts
+++ src/offerors/offerors.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OfferorsController } from './offerors.controller';
+import { OfferorsRepository } from './offerors.repository';
 import { OfferorsService } from './offerors.service';
 
 @Module({
   controllers: [OfferorsController],
-  providers: [OfferorsService],
+  providers: [OfferorsService, OfferorsRepository],
 })
 export class OfferorsModule {}
