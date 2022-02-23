a0ce06294c69076e155a22b24d215d3a9c878a88
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-02-23 16:52:33 +0100

    Update requests.module.ts
    Importing reservation to requests module and its exported providers.

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ReservationsModule } from 'src/reservations/reservations.module';
 import { RequestsController } from './requests.controller';
 import { RequestsRepository } from './requests.repository';
 import { RequestsService } from './requests.service';
 
 @Module({
+  imports: [ReservationsModule],
   controllers: [RequestsController],
   providers: [RequestsService, RequestsRepository],
 })
