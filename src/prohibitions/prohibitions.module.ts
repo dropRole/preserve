75a76894b4e74b875022d729d9a5e3f4ed46ecf3
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-02-25 14:05:07 +0100

    Generated prohibitions module

---
+++ src/prohibitions/prohibitions.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ProhibitionsController } from './prohibitions.controller';
+import { ProhibitionsService } from './prohibitions.service';
+import { ProhibitionsRepository } from './prohibitons.repository';
+
+@Module({
+  controllers: [ProhibitionsController],
+  providers: [ProhibitionsService, ProhibitionsRepository]
+})
+export class ProhibitionsModule {}
