010e57ae8434237608ce7243c8d85081824f04b1
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-02-25 16:08:21 +0100

    Update requests.service.ts
    Renamed dependency of requests repository property. Implemented logic of throwing NotFoundException exception when delete operation is performed on non-existing reservation request.

--- src/requests/requests.service.ts
+++ src/requests/requests.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RequestForReservationDTO } from './dto/request-for-reservartion.dto';
 import { RequestsRepository } from './requests.repository';
@@ -11,27 +15,30 @@ export class RequestsService {
   constructor(
     private reservationsService: ReservationsService,
     @InjectRepository(RequestsRepository)
-    private requestRepository: RequestsRepository,
+    private requestsRepository: RequestsRepository,
   ) {}
 
   requestForReservation(
     requestForReservationDTO: RequestForReservationDTO,
   ): Promise<Request> {
-    return this.requestRepository.insertRequest(requestForReservationDTO);
+    return this.requestsRepository.insertRequest(requestForReservationDTO);
   }
 
   getRequests(getRequestsFilterDTO: GetRequestsFilterDTO): Promise<Request[]> {
-    return this.requestRepository.selectRequests(getRequestsFilterDTO);
+    return this.requestsRepository.selectRequests(getRequestsFilterDTO);
   }
 
   getRequest(idRequests: string): Promise<Request> {
-    return this.requestRepository.selectRequest(idRequests);
+    return this.requestsRepository.selectRequest(idRequests);
   }
 
-  retreatRequest(idRequests: string): Promise<void> {
-    // if request hasn't been confirmed as reservation
+  async retreatRequest(idRequests: string): Promise<void> {
+    // if requests was not found
+    if (!(await this.requestsRepository.findOne(idRequests)))
+      throw new NotFoundException('Request was not found.');
     if (this.reservationsService.getReservation(idRequests))
+      // if request hasn't been confirmed as reservation
       throw new ConflictException('Request is already confirmed.');
-    return this.requestRepository.deleteRequest(idRequests);
+    return this.requestsRepository.deleteRequest(idRequests);
   }
 }
