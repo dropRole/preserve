a3d3e6f363cae7cb20cb22914da5370fcf259fca
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-03-02 17:52:13 +0100

    Update roles.decorator.ts

--- src/auth/roles.decorator.ts
+++ src/auth/roles.decorator.ts
@@ -2,4 +2,4 @@ import { SetMetadata } from '@nestjs/common';
 import { Role } from './enum/role.enum';
 
 export const ROLES = 'roles';
-export const Roles = (...roles: [Role]) => SetMetadata(ROLES, roles);
+export const Roles = (...roles: Role[]) => SetMetadata(ROLES, roles);
