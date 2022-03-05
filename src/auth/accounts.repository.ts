2e329f4e928574991bafa6e4b60c7fba664c5452
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-03-05 18:23:46 +0100

    Update accounts.repository.ts
    Integrated offeror and offeree account insertion methods.

--- src/auth/accounts.repository.ts
+++ src/auth/accounts.repository.ts
@@ -7,26 +7,7 @@ import { ConflictException } from '@nestjs/common';
 
 @EntityRepository(Account)
 export class AccountsRepository extends Repository<Account> {
-  async insertOffereeAccount(
-    authCredentialsDTO: AuthCredentialsDTO,
-  ): Promise<boolean> {
-    const { username, password } = authCredentialsDTO;
-    // generate bcrypt hash substrated from password and salt
-    const salt = await bcrypt.genSalt();
-    const hash = await bcrypt.hash(password, salt);
-    const account = this.create({ username, password: hash });
-    try {
-      await this.insert(account);
-    } catch (error) {
-      // if offeree username is duplicate
-      return false;
-    }
-    return true;
-  }
-
-  async insertOfferorAccount(
-    authCredentialsDTO: AuthCredentialsDTO,
-  ): Promise<void> {
+  async insertAccount(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
     const { username, password } = authCredentialsDTO;
     // generate bcrypt hash substrated from password and salt
     const salt = await bcrypt.genSalt();
