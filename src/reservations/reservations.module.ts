74ee9a5515b27bec5e86db7ee926f38073254f6a
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-03-30 18:58:07 +0200

    Update reservations.module.ts
    Imported requests module and inputted its service and repository providers.

--- src/reservations/reservations.module.ts
+++ src/reservations/reservations.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationsService } from './reservations.service';
+import { RequestsModule } from '../requests/requests.module';
+import { RequestsService } from '../requests/requests.service';
+import { RequestsRepository } from 'src/requests/requests.repository';
 
 @Module({
+  imports: [RequestsModule],
   controllers: [ReservationsController],
-  providers: [ReservationsService, ReservationsRepository],
+  providers: [
+    ReservationsService,
+    ReservationsRepository,
+    RequestsService,
+    RequestsRepository,
+  ],
   exports: [ReservationsService, ReservationsRepository],
 })
 export class ReservationsModule {}
