b6dd46fb9de47b9354e0951020855b545cc7c929
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-03-30 18:57:04 +0200

    Update reservation.entity.ts
    Defined key column of the table. Interposed JoinColumn decorator on one-to-one request relation.

--- src/reservations/reservation.entity.ts
+++ src/reservations/reservation.entity.ts
@@ -1,9 +1,19 @@
-import { Column, Entity, OneToOne } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Request } from 'src/requests/request.entity';
 
 @Entity('reservations')
 export class Reservation {
+  @PrimaryGeneratedColumn('uuid')
+  idReservations: string;
+
   @OneToOne((_type) => Request, { eager: true })
+  @JoinColumn()
   request: Request;
 
   @Column()
