56e0fa59fc350bad09558367f1591dd6779df5da
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-04-02 16:28:17 +0200

    Update accounts.repository.ts
    Interpolated Privilege argument to insertAccount and delegated to Account instance creation.

--- src/auth/accounts.repository.ts
+++ src/auth/accounts.repository.ts
@@ -4,17 +4,19 @@ import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 import * as bcrypt from 'bcrypt';
 import { ErrorCode } from './enum/error-code.enum';
 import { ConflictException } from '@nestjs/common';
+import { Privilege } from './enum/privilege.enum';
 
 @EntityRepository(Account)
 export class AccountsRepository extends Repository<Account> {
   async insertAccount(
     authCredentialsDTO: AuthCredentialsDTO,
+    privilege: Privilege,
   ): Promise<Account> {
     const { username, password } = authCredentialsDTO;
     // generate bcrypt hash substrated from password and salt
     const salt = await bcrypt.genSalt();
     const hash = await bcrypt.hash(password, salt);
-    const account = this.create({ username, password: hash });
+    const account = this.create({ username, password: hash, privilege });
     try {
       await this.insert(account);
     } catch (error) {
