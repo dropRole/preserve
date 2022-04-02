7d432d50ef269b5bde9b23be7bd566bd0b4f17c5
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-04-02 16:22:19 +0200

    Update jwt.strategy.ts
    Unaffiliated Connection library and its instantiation within the constructor. Dependently injecting, constructor based, custom AccountsRepository provider instance.

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,21 +1,21 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { Connection } from 'typeorm';
 import { Account } from './account.entity';
 import { AccountsRepository } from './accounts.repository';
 import { JWTPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  private accountsRepository: AccountsRepository;
-  constructor(private connection: Connection) {
+  constructor(
+    @InjectRepository(AccountsRepository)
+    private accountsRepository: AccountsRepository,
+  ) {
     super({
       secretOrKey: 'preserveapp',
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
-    this.accountsRepository =
-      this.connection.getCustomRepository(AccountsRepository);
   }
 
   async validate(payload: JWTPayload) {
