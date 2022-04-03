6580eaed9aefa4f97e8eaa5364a4112915f4b3b4
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-04-03 16:55:38 +0200

    Update reservations.service.ts
    Implemented getReservation within deleteReservation instead of repositories findOne method.

--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -49,11 +49,7 @@ export class ReservationsService {
   }
 
   async deleteReservation(account: Account, idRequests: string): Promise<void> {
-    const reservation = await this.reservationsRepository.findOne({
-      idReservations: idRequests,
-    });
-    // if reservation was not found
-    if (!reservation) throw new NotFoundException('Reservation was not found.');
+    const reservation = await this.getReservation(account, idRequests);
     // if reservation wasn't confirmed by the account
     if (reservation.request.offeror.account !== account)
       throw new UnauthorizedException(
