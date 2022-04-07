8698bbaf3c0e23042b6dd1bb02a1468b226fe4f3
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-04-07 14:37:34 +0200

    Update requests.controller.ts
    Implemented default AuthGuard guard at the controller scope. Abolished route path for POST route handler.Interpolated account argument decorated with GetAccount that populated it with Account instance from the incoming request object and passed as an argument to requestForReservation. Argument requestReservationDTO decorated with Body.

--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
 import { RequestForReservationDTO } from './dto/request-for-reservartion.dto';
 import { RequestsService } from './requests.service';
 import { Request } from './request.entity';
@@ -7,17 +7,23 @@ import { Privileges } from 'src/auth/privilege.decorator';
 import { Privilege } from 'src/auth/enum/privilege.enum';
 import { GetAccount } from 'src/auth/get-account.decorator';
 import { Account } from 'src/auth/account.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('request')
+@UseGuards(AuthGuard())
 export class RequestsController {
   constructor(private requestsService: RequestsService) {}
 
-  @Post(':idRequests')
+  @Post()
   @Privileges(Privilege.Offeree)
   requestForReservation(
-    requestForReservationDTO: RequestForReservationDTO,
+    @GetAccount() account: Account,
+    @Body() requestForReservationDTO: RequestForReservationDTO,
   ): Promise<Request> {
-    return this.requestsService.requestForReservation(requestForReservationDTO);
+    return this.requestsService.requestForReservation(
+      account,
+      requestForReservationDTO,
+    );
   }
 
   @Get()
