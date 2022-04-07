6ef1d8370555dc81f12a0173eab647acba052691
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-04-07 14:50:27 +0200

    Update reservations.module.ts
    Imported static OfferorsModule and OffereesModule. Unprovided RequestsService.

--- src/reservations/reservations.module.ts
+++ src/reservations/reservations.module.ts
@@ -6,14 +6,20 @@ import { RequestsModule } from '../requests/requests.module';
 import { RequestsService } from '../requests/requests.service';
 import { RequestsRepository } from 'src/requests/requests.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { OfferorsModule } from 'src/offerors/offerors.module';
+import { OfferorsService } from 'src/offerors/offerors.service';
+import { OffereesModule } from 'src/offerees/offerees.module';
+import { OffereesService } from 'src/offerees/offerees.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ReservationsRepository, RequestsRepository]),
     forwardRef(() => RequestsModule),
+    OfferorsModule,
+    OffereesModule,
   ],
   controllers: [ReservationsController],
-  providers: [ReservationsService, RequestsService],
+  providers: [ReservationsService],
   exports: [ReservationsService],
 })
 export class ReservationsModule {}
