a9ec25c0cec5660c37b172bac79f90220d9d6672
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-04-18 15:24:31 +0200

    Update requests.controller.ts
    Fixed typo within controllers route path prefix. Interpolated arguments within argument Param and route handler Delete decorators.

--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { RequestForReservationDTO } from './dto/request-for-reservartion.dto';
 import { RequestsService } from './requests.service';
 import { Request } from './request.entity';
@@ -9,7 +18,7 @@ import { GetAccount } from 'src/auth/get-account.decorator';
 import { Account } from 'src/auth/account.entity';
 import { AuthGuard } from '@nestjs/passport';
 
-@Controller('request')
+@Controller('requests')
 @UseGuards(AuthGuard())
 export class RequestsController {
   constructor(private requestsService: RequestsService) {}
@@ -39,12 +48,12 @@ export class RequestsController {
   @Privileges(Privilege.Offeror, Privilege.Offeree)
   getRequestById(
     @GetAccount() account: Account,
-    @Param() idRequests: string,
+    @Param('idRequests') idRequests: string,
   ): Promise<Request> {
     return this.requestsService.getRequestById(account, idRequests);
   }
 
-  @Delete()
+  @Delete('/:idRequests')
   @Privileges(Privilege.Offeree)
   retreatRequest(
     @GetAccount() account: Account,
