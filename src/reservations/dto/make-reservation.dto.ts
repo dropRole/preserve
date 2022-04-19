04f35f4143656a3654238f0a78d4679b014512f2
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-04-19 14:03:37 +0200

    Update make-reservation.dto.ts
    Eradicated code and confirmedAt. Inverse of request to idRequests.

--- src/reservations/dto/make-reservation.dto.ts
+++ src/reservations/dto/make-reservation.dto.ts
@@ -1,14 +1,7 @@
 import { IsNotEmpty, IsString } from 'class-validator';
-import { Request } from '../../requests/request.entity';
 
 export class MakeReservationDTO {
-  request: Request;
-
   @IsString()
   @IsNotEmpty()
-  code: string;
-
-  @IsString()
-  @IsNotEmpty()
-  confirmedAt: string;
+  idRequests: string;
 }
