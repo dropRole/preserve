36f050be15d87339aeeb45585339619df33e46bf
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-04-19 19:50:42 +0200

    Update complaint.entity.ts
    Interpolated JoinColumn with options object to many to one request, one to one account and one to one counteredComplaint relations. Defined data types for content, written and updated columns.

--- src/complaints/complaint.entity.ts
+++ src/complaints/complaint.entity.ts
@@ -17,26 +17,31 @@ export class Complaint {
   @ManyToOne((_type) => Request, (request) => request.complaints, {
     eager: false,
   })
+  @JoinColumn({ name: 'idRequests', referencedColumnName: 'idRequests' })
   request: Request;
 
   @OneToOne((_type) => Account, (account) => account.complaints, {
     eager: true,
   })
+  @JoinColumn({ name: 'username', referencedColumnName: 'username' })
   account: Account;
 
   @OneToOne((_type) => Complaint, (complaint) => complaint.counteredComplaint, {
     eager: false,
     nullable: true,
   })
-  @JoinColumn()
+  @JoinColumn({
+    name: 'idCounteredComplaints',
+    referencedColumnName: 'idComplaints',
+  })
   counteredComplaint: Complaint;
 
-  @Column()
+  @Column({ type: 'text' })
   content: string;
 
-  @Column()
+  @Column({ type: 'timestamp' })
   written: string;
 
-  @Column({ nullable: true })
+  @Column({ type: 'timestamp', nullable: true })
   updated: string;
 }
