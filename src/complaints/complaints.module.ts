d29d9e13881e9d13d25045c635df24403212b506
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-04-20 16:11:22 +0200

    Update complaints.module.ts
    Statically binded ReservationsModule module.

--- src/complaints/complaints.module.ts
+++ src/complaints/complaints.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { ReservationsModule } from 'src/reservations/reservations.module';
 import { ComplaintsController } from './complaints.controller';
 import { ComplaintsRepository } from './complaints.repository';
 import { ComplaintsService } from './complaints.service';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forFeature([ComplaintsRepository])],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([ComplaintsRepository]),
+    ReservationsModule,
+  ],
   controllers: [ComplaintsController],
   providers: [ComplaintsService],
 })
