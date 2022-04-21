745c682451c61994eb98b278db1ff5160df3b09e
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-04-21 13:12:17 +0200

    Revert "Update app.module.ts"
    This reverts commit 42fde73bb21671653b1051a69dde777dd35880ed.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OfferorsModule } from './offerors/offerors.module';
 import { OffereesModule } from './offerees/offerees.module';
 import { ProhibitionsModule } from './prohibitions/prohibitions.module';
-import { PrivilegesGuard } from './auth/privileges.guard';
 
 @Module({
   imports: [
@@ -28,12 +27,6 @@ import { PrivilegesGuard } from './auth/privileges.guard';
     OfferorsModule,
     OffereesModule,
     ProhibitionsModule,
-  ],
-  providers: [
-    {
-      provide: 'APP_GUARD',
-      useClass: PrivilegesGuard,
-    },
   ],
 })
 export class AppModule {}
