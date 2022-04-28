26a2c0a040214d84d0f0c93e2bbdeb9c1f5c5614
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-04-28 14:11:27 +0200

    Update prohibitions.module.ts
    Imported OfferorsModule and OffereesModule modules.

--- src/prohibitions/prohibitions.module.ts
+++ src/prohibitions/prohibitions.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { OffereesModule } from 'src/offerees/offerees.module';
+import { OfferorsModule } from 'src/offerors/offerors.module';
 import { ProhibitionsController } from './prohibitions.controller';
 import { ProhibitionsService } from './prohibitions.service';
 import { ProhibitionsRepository } from './prohibitons.repository';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forFeature([ProhibitionsRepository])],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([ProhibitionsRepository]),
+    OfferorsModule,
+    OffereesModule,
+  ],
   controllers: [ProhibitionsController],
   providers: [ProhibitionsService],
 })
