350db7d8057322096ba3120d1691c5640539f18d
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-05-13 15:28:13 +0200

    Update offeror.entity.ts
    Set account and prohibitions relations falsy eagerness.

--- src/offerors/offeror.entity.ts
+++ src/offerors/offeror.entity.ts
@@ -43,7 +43,7 @@ export class Offeror {
   @Check('timeliness IN(5, 6, 7, 8, 9, 10)')
   timeliness: number;
 
-  @OneToOne((_type) => Account, { eager: true })
+  @OneToOne((_type) => Account, { eager: false })
   @JoinColumn({ name: 'username', referencedColumnName: 'username' })
   account: Account;
 
@@ -52,7 +52,7 @@ export class Offeror {
   requests: Request[];
 
   @OneToMany((_type) => Prohibition, (prohibition) => prohibition.offeror, {
-    eager: true,
+    eager: false,
   })
   @JoinColumn({
     name: 'idProhibitions',
