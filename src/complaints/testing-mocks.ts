d3937eba1f9883d3b77415fae16f4cc1a7ceebe5
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-05-24 21:06:35 +0200

    Update testing-mocks.ts
    Defined mock for reSubmitComplaintDTO data transfer object. Defined mock methods for mock complaints repository provider.

--- src/complaints/testing-mocks.ts
+++ src/complaints/testing-mocks.ts
@@ -3,6 +3,7 @@ import { Privilege } from '../auth/enum/privilege.enum';
 import { SubmitComplaintDTO } from './dto/submit-complaint';
 import { Reservation } from '../reservations/reservation.entity';
 import { Complaint } from './complaint.entity';
+import { ReSubmitComplaintDTO } from './dto/re-submit-complaint.dto';
 
 export const mockAccount = new Account();
 mockAccount.username = 'dropRole';
@@ -14,6 +15,10 @@ export const mockSubmitComplaintDTO = new SubmitComplaintDTO();
 mockSubmitComplaintDTO.idReservations = '4b320768-69a9-48a5-adaf-c102d6c093ae';
 mockSubmitComplaintDTO.content = 'Mock complaint';
 
+export const mockReSubmitComplaintDTO = new ReSubmitComplaintDTO()
+mockReSubmitComplaintDTO.idComplaints = '62b0bdd5-f265-4f4d-a96a-449a7e363fa7'
+mockReSubmitComplaintDTO.content = 'Amended content.'
+
 export const mockReservation = new Reservation();
 mockReservation.code = '275d20b3';
 mockReservation.request = null;
@@ -22,7 +27,10 @@ mockReservation.complaints = [];
 
 export const mockComplaintsRepository = () => ({
   findOne: jest.fn().mockResolvedValue(undefined),
-  insertComplaint: jest.fn().mockResolvedValue(Complaint),
+  insertComplaint: jest.fn().mockResolvedValue(Promise.resolve(Complaint)),
+  selectComplaints: jest.fn().mockResolvedValue(Promise.resolve([Complaint])),
+  reComplain: jest.fn().mockResolvedValue(Promise.resolve()),
+  withdrawComplaint: jest.fn().mockResolvedValue(Promise.resolve()),
 });
 
 export const mockReservationsService = {
