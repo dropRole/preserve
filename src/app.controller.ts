bedb08baf3b605d1ec029d7220bb7a131614e58d
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-06-23 16:21:15 +0200

    Update app.controller.ts
    Defined GET request handler rendering homepage of the signed in offeree.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,7 @@ import { Response } from 'express';
 
 @Controller()
 export class AppController {
+
   @Get()
   root(@Res() res: Response) {
     return res.render('index');
@@ -16,5 +17,10 @@ export class AppController {
   @Get('/signin')
   signin(@Res() res: Response) {
     return res.render('signin');
+  }
+
+  @Get('/offeree')
+  offereeHomePage(@Res() res: Response) {
+    return res.render('homepage');
   }
 }
