b5849a40366c19db01e1fb1109af41ace008d16e
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-06-28 15:08:32 +0200

    Update homepage.js
    Amended condition for adding click event listener to offeror search button to response status equality to 200. Callback function gets offerors by established clients geolocation and exposed them through the modal.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -12,10 +12,17 @@ const establishGeolocation = () => {
         `/geolocation/geocoding?lat=${geolocation.coords.latitude}&long=${geolocation.coords.longitude}`,
         requestOptions,
       );
-      const geocoding = await response.json();
-      document.getElementById(
-        'geolocation',
-      ).textContent = `Search offerors for ${geocoding.address.town}`;
+
+      // if response successfully returned
+      if (response.status === 200) {
+        const geocoding = await response.json();
+        const estGeoMun = document.getElementById('estGeoMun');
+        const srchOffrBtn = document.getElementById('srchOffrBtn');
+        estGeoMun.textContent = `Search offerors for ${geocoding.address.town}`;
+        srchOffrBtn.disabled = false;
+        srchOffrBtn.dataset.municipality = geocoding.address.town;
+        srchOffrBtn.addEventListener('click', getOfferorsByGeolocation);
+      }
     },
     () => console.warn('Geolocation API is not present.'),
     {
@@ -24,3 +31,98 @@ const establishGeolocation = () => {
   );
 };
 establishGeolocation();
+
+// create and return table element populated with offeror data
+const createOfferorTable = (offerors) => {
+  const table = document.createElement('table'),
+    tHead = document.createElement('thead'),
+    tHdRow = document.createElement('tr'),
+    tHdCellName = document.createElement('th'),
+    tHdCellAddress = document.createElement('th'),
+    tHdCellBusinessHours = document.createElement('th'),
+    tHdCellResponsiveness = document.createElement('th'),
+    tHdCellCompliance = document.createElement('th'),
+    tHdCellTimeliness = document.createElement('th'),
+    tBody = document.createElement('tbody');
+
+  table.classList = 'table table-borderless';
+
+  tHdCellName.textContent = 'Name';
+  tHdCellAddress.textContent = 'Address';
+  tHdCellBusinessHours.textContent = 'Business hours';
+  tHdCellResponsiveness.textContent = 'Responsiveness(mark)';
+  tHdCellCompliance.textContent = 'Compliance(mark)';
+  tHdCellTimeliness.textContent = 'Timeliness(mark)';
+
+  tHdRow.append(tHdCellName);
+  tHdRow.append(tHdCellAddress);
+  tHdRow.append(tHdCellBusinessHours);
+  tHdRow.append(tHdCellResponsiveness);
+  tHdRow.append(tHdCellCompliance);
+  tHdRow.append(tHdCellTimeliness);
+
+  tHead.append(tHdRow);
+
+  offerors.forEach((offeror) => {
+    const tBdRow = document.createElement('tr'),
+      tBdCellName = document.createElement('td'),
+      tBdCellAddress = document.createElement('td'),
+      tBdCellBusinessHours = document.createElement('td'),
+      tBdCellResponsiveness = document.createElement('td'),
+      tBdCellCompliance = document.createElement('td'),
+      tBdCellTimeliness = document.createElement('td');
+
+    tBdCellName.textContent = offeror.name;
+    tBdCellAddress.textContent = offeror.address;
+    tBdCellBusinessHours.textContent = offeror.businessHours;
+    tBdCellResponsiveness.textContent = offeror.responsiveness;
+    tBdCellCompliance.textContent = offeror.compliance;
+    tBdCellTimeliness.textContent = offeror.timeliness;
+
+    tBdRow.append(tBdCellName);
+    tBdRow.append(tBdCellAddress);
+    tBdRow.append(tBdCellBusinessHours);
+    tBdRow.append(tBdCellResponsiveness);
+    tBdRow.append(tBdCellCompliance);
+    tBdRow.append(tBdCellTimeliness);
+    tBody.append(tBdRow);
+  });
+
+  table.append(tHead);
+  table.append(tBody);
+
+  return table;
+};
+
+// fetch an offeror list based on the offeree geolocation and render the DocumentFragment
+const getOfferorsByGeolocation = async (event) => {
+  const municipality = event.target.dataset.municipality;
+
+  const headers = new Headers();
+  headers.append('Authorization', `Bearer ${sessionStorage.getItem('JWT')}`);
+
+  const requestOptions = {
+    method: 'GET',
+    headers: headers,
+  };
+
+  const response = await fetch(
+    `/offerors/geolocation/${municipality}`,
+    requestOptions,
+  );
+
+  const offerors = await response.json();
+
+  const documentFragment = new DocumentFragment(),
+    div = document.createElement('div'),
+    table = createOfferorTable(offerors),
+    modalBody = document.querySelector('.modal-body');
+
+  div.classList = 'table-responsive';
+
+  div.append(table)
+  documentFragment.append(div);
+
+  modalBody.innerHTML = '';
+  modalBody.append(documentFragment);
+};
