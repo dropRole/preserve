3fc7ea90c9f408525a7d200b1db0b8dd5dd6dabf
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-07-07 15:33:03 +0200

    Update reservations.controller.ts
    Granted privilege to offeree accounts to approach reservations resources.

--- src/reservations/reservations.controller.ts
+++ src/reservations/reservations.controller.ts
@@ -34,7 +34,7 @@ export class ReservationsController {
   }
 
   @Get()
-  @Privileges(Privilege.Admin, Privilege.Offeror)
+  @Privileges(Privilege.Admin, Privilege.Offeror, Privilege.Offeree)
   getReservations(
     @GetAccount() account: Account,
     @Query() getReservationsFilterDTO: GetReservationsFilterDTO,
