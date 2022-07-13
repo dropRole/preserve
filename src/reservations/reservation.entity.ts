9a9b4e3877b2637adb89764b7513c3ec07fc083e
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2022-07-13 18:46:57 +0200

    Update reservation.entity.ts
    Substituted the NOW with the , wrapped up with an arrow expression, CURRENT_TIMESTAMP constant as the default value of confirmedAt column.

--- src/reservations/reservation.entity.ts
+++ src/reservations/reservation.entity.ts
@@ -14,7 +14,7 @@ export class Reservation {
   @Column({ type: 'character varying', length: 8 })
   code: string;
 
-  @Column({ type: 'timestamp', default: 'now' })
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   confirmedAt: Date;
 
   @OneToMany((_type) => Complaint, (complaint) => complaint.reservation, {
